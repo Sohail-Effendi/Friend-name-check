f140eb44c8e52f463b1afec7778b2d1258c9d3ae
Sohail-Effendi <160319647+Sohail-Effendi@users.noreply.github.com>
2024-05-05 16:47:43 +0500

    Friend name check game

---
+++ FriendCheck.js
@@ -0,0 +1,16 @@
+var isfriend = "Maaz";
+var fathername = "Hanif";
+if (isfriend === "Maaz" && fathername === "Hanif") {
+    console.log("".concat(isfriend, " is your friend."));
+}
+else {
+    console.log("".concat(isfriend, " is not your friend"));
+}
+var friend = "Ali";
+var FatherN = "Ahmed";
+if (friend === "Ali" || FatherN === "Ahmed") {
+    console.log("".concat(friend, " is your friend."));
+}
+else {
+    console.log("".concat(friend, " is not your friend"));
+}
---
+++ FriendCheck.ts
@@ -0,0 +1,10 @@
+let isfriend : string ="Maaz";
+let fathername: string = "Hanif";
+if (isfriend === "Maaz" && fathername ==="Hanif") {console.log(`${isfriend} is your friend.`);
+} else { console.log(`${isfriend} is not your friend`);
+}
+let friend : string ="Ali";
+let FatherN: string = "Ahmed";
+if (friend === "Ali" || FatherN === "Ahmed") {console.log(`${friend} is your friend.`);
+} else { console.log(`${friend} is not your friend`);
+}
